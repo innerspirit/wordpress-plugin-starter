d10a028ba3b1fd53b43970af7015d5cdaf6faa52
codekraft-studio <info@codekraft.it>
2016-10-18 17:26:41 +0200

    package rebuild

--- wordpress-plugin-starter.php
+++ wordpress-plugin-starter.php
@@ -21,10 +21,16 @@
  *
  */
 
+// Block direct access to file
+defined( 'ABSPATH' ) or die( 'Not Authorized!' );
+
+// Plugin Defines
 define( "WPS_FILE", __FILE__ );
 define( "WPS_DIRECTORY", dirname(__FILE__) );
+define( "WPS_TEXT_DOMAIN", dirname(__FILE__) );
 define( "WPS_DIRECTORY_BASENAME", plugin_basename( WPS_FILE ) );
 define( "WPS_DIRECTORY_PATH", plugin_dir_path( WPS_FILE ) );
 define( "WPS_DIRECTORY_URL", plugins_url( null, WPS_FILE ) );
 
+// Require the main class file
 require_once( WPS_DIRECTORY . '/include/main-class.php' );
